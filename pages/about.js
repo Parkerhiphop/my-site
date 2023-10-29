093066df7488a7c056cb67d6a674be6864a1a49c
Parker <parkerhiphop027@gmail.com>
2023-10-30 01:57:04 +0800

    :recycle: Refactor: blog list ui
    display all posts by year

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,8 @@
 import { MDXLayoutRenderer } from '@/components/MDXComponents';
 import { getFileBySlug } from '@/lib/mdx';
 import { getCurrentLocale } from '@/lib/utils/getCurrentLocale';
+import useTranslation from 'next-translate/useTranslation';
+import Link from '@/components/Link';
 
 const DEFAULT_LAYOUT = 'AuthorLayout';
 
@@ -12,13 +14,25 @@ export async function getStaticProps({ locale, defaultLocale, locales }) {
 
 export default function About({ authorDetails, availableLocales }) {
   const { mdxSource, frontMatter } = authorDetails;
+  const { t } = useTranslation();
 
   return (
-    <MDXLayoutRenderer
-      layout={frontMatter.layout || DEFAULT_LAYOUT}
-      mdxSource={mdxSource}
-      frontMatter={frontMatter}
-      availableLocales={availableLocales}
-    />
+    <>
+      <MDXLayoutRenderer
+        layout={frontMatter.layout || DEFAULT_LAYOUT}
+        mdxSource={mdxSource}
+        frontMatter={frontMatter}
+        availableLocales={availableLocales}
+      />
+      <div className="flex justify-center text-base font-medium leading-6 mt-4">
+        <Link
+          href="/blog"
+          className="md:text-xl text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+          aria-label="all posts"
+        >
+          {t('common:all')} &rarr;
+        </Link>
+      </div>
+    </>
   );
 }
