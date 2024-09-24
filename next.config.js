299a1ebb7128b30832a86016ae94fe45a86f84c9
Parker <parkerhiphop027@gmail.com>
2024-09-24 15:18:31 +0800

    fix: rm useless file

--- next.config.js
+++ next.config.js
@@ -164,20 +164,11 @@ module.exports = nextTranslate(
     async redirects() {
       return redirects;
     },
-    webpack: (config, { dev, isServer }) => {
+    webpack: (config) => {
       config.module.rules.push({
         test: /\.svg$/,
         use: ['@svgr/webpack'],
       });
-      if (!dev && !isServer) {
-        // Replace React with Preact only in client production build
-        Object.assign(config.resolve.alias, {
-          'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-          react: 'preact/compat',
-          'react-dom/test-utils': 'preact/test-utils',
-          'react-dom': 'preact/compat',
-        });
-      }
 
       return config;
     },
