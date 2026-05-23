71d7b12d4ca2a7b75c588076e6abeb1cdd8d06f0
Parker <parkerhiphop027@gmail.com>
2026-05-24 02:28:53 +0900

    fix(localization): update search prompt to include description in multiple languages

--- components/SectionContainer.js
+++ components/SectionContainer.js
@@ -1,6 +1,6 @@
 export default function SectionContainer({ children }) {
   return (
-    <div className="mx-auto max-w-3xl md:max-w-5xl px-4 sm:px-6 xl:max-w-6xl xl:px-0">
+    <div className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-6xl xl:max-w-7xl xl:px-0">
       {children}
     </div>
   );
